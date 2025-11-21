993e254666d76e77159efc8a842224848b05f0d1
Muhammad Subhan <subyounas@gmail.com>
2025-11-21 17:37:42 +0000

    add yellow color to right of hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,6 +49,7 @@ export default function Home() {
               <div className="absolute inset-y-0 left-0 w-7/12 bg-secondary animate-slide-in-left" style={{ clipPath: 'ellipse(100% 75% at 0% 50%)' }}></div>
               <div className="absolute inset-y-0 left-0 w-1/4 bg-primary animate-slide-in-left" style={{ clipPath: 'ellipse(100% 75% at 0% 50%)', animationDelay: '0.2s' }}></div>
               <div className="absolute inset-y-0 right-0 w-1/2 bg-background animate-slide-in-right" style={{ clipPath: 'ellipse(100% 75% at 100% 50%)' }}></div>
+              <div className="absolute inset-y-0 right-0 w-1/4 bg-yellow-300 animate-slide-in-right" style={{ clipPath: 'ellipse(100% 75% at 100% 50%)', animationDelay: '0.2s' }}></div>
                 <div
                   className="absolute inset-0"
                   style={{
